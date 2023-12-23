9df06cdb3986572524ce37029dee385e95eedc74
Heidi <heidimccrudden@gmail.com>
2023-12-23 00:26:24 +0000

    Changed @WebServlet name and value to Controller. Edited doGet and doPost methods in Controller to throw ServletException.

--- src/main/java/servlet/Controller.java
+++ src/main/java/servlet/Controller.java
@@ -2,15 +2,26 @@ package servlet;
 
 import java.io.*;
 
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 // new project
-@WebServlet(name = "helloServlet", value = "/hello-servlet")
+@WebServlet(name = "Controller", value = "/Controller")
 public class Controller extends HttpServlet {
 
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    public void doGet(HttpServletRequest request, HttpServletResponse response)  throws ServletException, IOException {
 
     }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response)  throws ServletException, IOException {
+
+    }
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+    }
+
+
 
 }
