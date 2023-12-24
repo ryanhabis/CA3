c9b00de034365c6f323943273de2195afe75e91a
Heidi <heidimccrudden@gmail.com>
2023-12-24 15:15:51 +0000

    Created some other command classes we might later

---
+++ src/main/java/com/example/CA3/commands/ErrorCommand.java
@@ -0,0 +1,8 @@
+package com.example.CA3.commands;
+
+public class ErrorCommand implements Command{
+    @Override
+    public String execute() {
+        return null;
+    }
+}
---
+++ src/main/java/com/example/CA3/commands/SignupCommand.java
@@ -0,0 +1,8 @@
+package com.example.CA3.commands;
+
+public class SignupCommand implements Command{
+    @Override
+    public String execute() {
+        return null;
+    }
+}
