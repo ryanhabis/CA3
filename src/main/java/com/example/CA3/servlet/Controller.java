026126958f1f7a22a3386cca219e8d8001b3ff2c
Heidi <heidimccrudden@gmail.com>
2023-12-24 15:17:26 +0000

    Processed request in doGet and doPost in Controller. Got action from request and subsequently got the corresponding command for redirecting in processRequest method.

--- src/main/java/com/example/CA3/servlet/Controller.java
+++ src/main/java/com/example/CA3/servlet/Controller.java
@@ -2,6 +2,8 @@ package com.example.CA3.servlet;
 
 import java.io.*;
 
+import com.example.CA3.commands.Command;
+import com.example.CA3.commands.CommandFactory;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
@@ -11,15 +13,24 @@ public class Controller extends HttpServlet {
 
 
     public void doGet(HttpServletRequest request, HttpServletResponse response)  throws ServletException, IOException {
-
+        processRequest(request, response);
     }
 
     public void doPost(HttpServletRequest request, HttpServletResponse response)  throws ServletException, IOException {
-
+        processRequest(request, response);
     }
 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+        String continueTo = "index.jsp";
+        //get the requested action
+        String action = request.getParameter("action");
+        //turn it into a command
+        Command actionCommand = CommandFactory.setAsCommand(action, request, response);
+        //execute the command
+        continueTo = actionCommand.execute();
+        //redirect to the page specified
+        response.sendRedirect(continueTo);
     }
 
 
