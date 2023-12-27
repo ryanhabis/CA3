b24962105d983bcb50ac46d827c7384ea75b1695
Heidi <heidimccrudden@gmail.com>
2023-12-27 20:20:01 +0000

    Created dao exception in exception package

---
+++ src/main/java/com/example/CA3/exceptions/DaoException.java
@@ -0,0 +1,15 @@
+package com.example.CA3.exceptions;
+
+public class DaoException extends Throwable{
+
+    public DaoException() {
+    }
+
+    /**
+     * Method to create a DaoException when called. It will display the message passed to it.
+     * @param aMessage - the message displayed to indicate a Dao Exception occurred.
+     */
+    public DaoException(String aMessage) {
+        super(aMessage);
+    }
+}
