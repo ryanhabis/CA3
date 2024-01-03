bc4ddaea0f92945fab3c29b7d989c41df2034e66
Heidi <heidimccrudden@gmail.com>
2024-01-03 15:04:15 +0000

    Added sample login code to LoginCommand from Michelle's solution for login functionality - will be changed slightly later.

--- src/main/java/CA3/bookRentalSystem/commands/LoginCommand.java
+++ src/main/java/CA3/bookRentalSystem/commands/LoginCommand.java
@@ -18,9 +18,30 @@ public class LoginCommand implements Command {
     public String execute() {
 
         //create session to execute logincommand
-        HttpSession session = request.getSession();
+        HttpSession session = request.getSession(true);
         //set the string value to be returned
-        String continueTo = "login.jsp";
+        String continueTo = "../successfulLogin.jsp";
+
+        //taken from Michelle's solution
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        if (username != null && password != null && !username.isEmpty() && !password.isEmpty()) {
+            UserDao userDao = new UserDao("user_database");
+            User u = userDao.findUserByUsernamePassword(username, password);
+            if (u == null) {
+             //   continueTo = "error.jsp";
+                String error = "Incorrect credentials supplied. Please <a href=\"login.jsp\">try again.</a>";
+                session.setAttribute("errorMessage", error);
+            } else {
+                continueTo = "loginSuccessful.jsp";
+                session.setAttribute("username", username);
+                session.setAttribute("user", u);
+            }
+        } else {
+            continueTo = "error.jsp";
+            String error = "No username and/or password supplied. Please <a href=\"login.jsp\">try again.</a>";
+            session.setAttribute("errorMessage", error);
+        }
 
         return continueTo;
     }
