f21f3bfb6902428836e046c029d2c8106653783f
Heidi <heidimccrudden@gmail.com>
2024-01-05 14:57:38 +0000

    Refactored variables in LoginCommand execute method and added comments explaining what is going on. Rerouted to loginSuccessful.jsp page if successful. Changed database used to create dao to bookrentalsystem.

--- src/main/java/CA3/bookRentalSystem/commands/LoginCommand.java
+++ src/main/java/CA3/bookRentalSystem/commands/LoginCommand.java
@@ -27,20 +27,23 @@ public class LoginCommand implements Command {
         //taken from Michelle's solution
         String username = request.getParameter("username");
         String password = request.getParameter("password");
+        //if fields entered are not empty and are not null
         if (username != null && password != null && !username.isEmpty() && !password.isEmpty()) {
-            UserDao userDao = new UserDao("user_database");
-            User u = userDao.findUserByUsernamePassword(username, password);
-            if (u == null) {
-             //   continueTo = "error.jsp";
+            //make a new userdao using the regular database
+            UserDao userDao = new UserDao("bookrentalsystem");
+            //retrieve User matching details entered
+            User userFound = userDao.findUserByUsernameAndPassword(username, password);
+            if (userFound == null) { //if no matching user is found
+             //   continueTo = "error.jsp"; //go to error page
                 String error = "Incorrect credentials supplied. Please <a href=\"login.jsp\">try again.</a>";
                 session.setAttribute("errorMessage", error);
             } else {
-                continueTo = "loginSuccessful.jsp";
-                session.setAttribute("username", username);
-                session.setAttribute("user", u);
+                continueTo = "loginSuccessful.jsp"; //otherwise, continue to the loginSuccessful page
+                session.setAttribute("username", username); //set the username variable to the current session
+                session.setAttribute("user", userFound); //set the user object to the current session
             }
         } else {
-            continueTo = "error.jsp";
+           // continueTo = "error.jsp"; ////go to error page
             String error = "No username and/or password supplied. Please <a href=\"login.jsp\">try again.</a>";
             session.setAttribute("errorMessage", error);
         }
