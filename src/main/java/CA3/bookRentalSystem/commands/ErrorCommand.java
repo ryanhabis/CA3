e096e7c4f74b3b8e0c577c7431e6474c1047c675
Heidi <heidimccrudden@gmail.com>
2024-01-05 21:01:47 +0000

    set continueTo string in ErrorCommand to redirect to error.jsp

--- src/main/java/CA3/bookRentalSystem/commands/ErrorCommand.java
+++ src/main/java/CA3/bookRentalSystem/commands/ErrorCommand.java
@@ -3,6 +3,9 @@ package CA3.bookRentalSystem.commands;
 public class ErrorCommand implements Command {
     @Override
     public String execute() {
+        //set the string value to be returned
+        String continueTo = "../error.jsp";
 
+        return continueTo;
     }
 }
