7124fe58269af51297ba4b54e0f79a2deeecc179
EvanM1603 <evanmcdonnell09@gmail.com>
2024-01-06 23:20:58 +0000

    Combined the BookDao.java and BookDaoAdmin.java

--- src/main/java/CA3/bookRentalSystem/repositories/BookDao.java
+++ src/main/java/CA3/bookRentalSystem/repositories/BookDao.java
@@ -11,7 +11,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 
-public class BookDao extends Dao implements BookDaoInterface {
+public class BookDao extends Dao implements BookDaoInterface, BookDaoAdminInterface {
 
         public BookDao(Connection conn) {
             super(conn);
@@ -159,5 +159,97 @@ public class BookDao extends Dao implements BookDaoInterface {
 
             return bookId;
         }
+
+    @Override
+    public int addBook(int bookId, int genreId, String title, String description, String author, int quantityInstock, double bookPrice) {
+        return 0;
+    }
+
+    @Override
+    public int updateStock(int bookId, int quantityInStock) {
+        return 0;
+    }
+
+    @Override
+    public int checkStock(int bookId) {
+        return 0;
+    }
+
+    @Override
+    public Book getBookByBookId(int bookId) {
+        return null;
+    }
+
+    @Override
+    public Book getBookByBookTitle(String title) {
+        return null;
+    }
+
+    // BookDao to reserve a copy (if there are no copies available, return a fail code indicating a loan could not be made)
+    public boolean reserveCopy (Book b) {
+
+        //check book has enough stock
+        Connection conn = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        try {
+            //Getting connection
+            conn = getConnection();
+            //Creating query
+            String query = "select * from books where bookId = ? and quantityInStock > 0 ";
+            //Prepare & execute query
+            ps = conn.prepareStatement(query);
+            int id = b.getBookId();
+            ps.setInt(1, id);
+
+            rs = ps.executeQuery();
+
+            while (rs.next()) {
+
+                //Setting values to correct columns
+                b.setBookId(rs.getInt("bookId"));
+                b.setGenreId(rs.getInt("genreId"));
+                b.setTitle(rs.getString("title"));
+                b.setDescription(rs.getString("description"));
+                b.setAuthor(rs.getString("author"));
+                b.setQuantityInStock(rs.getInt("quantityInStock"));
+                b.setBookPrice(rs.getDouble("bookPrice"));
+
+            }
+
+        } catch (SQLException | DaoException e) {
+            System.out.println("SQL exception: " +e.getMessage());
+        }
+
+        //Closing Connection
+        finally {
+            if(rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException e) {
+                    System.out.println("Exception message: " + e.getMessage());
+                    System.out.println("Issue when closing result set: ");
+                }
+            }
+            if(ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException e) {
+                    System.out.println("Exception message: " + e.getMessage());
+                    System.out.println("Issue when closing prepared statement: ");
+                }
+            }
+            if(conn != null) {
+                try {
+                    freeConnection(conn);
+                } catch (DaoException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+
+        return b.getBookId() != -1;
+    }
 
 }
