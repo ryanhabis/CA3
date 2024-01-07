96935af3760d18c6446236519cf94d114d7d6a56
Heidi <heidimccrudden@gmail.com>
2024-01-07 11:17:10 +0000

    removed closing of connections in userdao methods again

--- src/main/java/CA3/bookRentalSystem/commands/ErrorCommand.java
+++ src/main/java/CA3/bookRentalSystem/commands/ErrorCommand.java
@@ -1,6 +1,20 @@
 package CA3.bookRentalSystem.commands;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
 public class ErrorCommand implements Command {
+
+    private HttpServletRequest request;
+    private HttpServletResponse response;
+    private String errorMessage;
+
+    public ErrorCommand(HttpServletRequest request, HttpServletResponse response, String errorMessage) {
+        this.request = request;
+        this.response = response;
+        this.errorMessage = errorMessage;
+    }
+
     @Override
     public String execute() {
         //set the string value to be returned
--- src/main/java/CA3/bookRentalSystem/commands/SignupCommand.java
+++
@@ -1,8 +0,0 @@
-package CA3.bookRentalSystem.commands;
-
-public class SignupCommand implements Command {
-    @Override
-    public String execute() {
-        return null;
-    }
-}
