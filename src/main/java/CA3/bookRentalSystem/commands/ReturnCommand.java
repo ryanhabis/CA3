6582c32aad4de9464f1b82177cde093f1dff3189
EvanM1603 <evanmcdonnell09@gmail.com>
2024-01-07 23:11:40 +0000

    Created successfulReturn.jsp, updated BorrowCommand.java, ReturnCommand.java should now be working, updated SearchCommand.java and ViewBooksCommand.java

--- src/main/java/CA3/bookRentalSystem/commands/ReturnCommand.java
+++ src/main/java/CA3/bookRentalSystem/commands/ReturnCommand.java
@@ -4,14 +4,13 @@ package CA3.bookRentalSystem.commands;
  * Reference: Michelle's notes
  **/
 
-import CA3.bookRentalSystem.rental.Book;
-import CA3.bookRentalSystem.repositories.BookDaoInterface;
+import CA3.bookRentalSystem.rental.User;
 import CA3.bookRentalSystem.repositories.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
-public class ReturnCommand implements Command{
+public class ReturnCommand implements Command {
     private HttpServletRequest request;
     private HttpServletResponse response;
 
@@ -21,7 +20,38 @@ public class ReturnCommand implements Command{
     }
 
     @Override
-    public String execute(){
-        return null;
+    public String execute() {
+
+        String continueTo = "../return.jsp";
+
+        HttpSession session = request.getSession();
+
+        //Getting logged in user from session
+        User loggedIn = (User) session.getAttribute("user");
+        //Getting loanId from session
+        Integer loanId = (Integer) session.getAttribute("loanId");
+
+        //Checking if user is not logged in or if loan doesn't exist
+        if (loggedIn == null || loanId == null) {
+            return "redirect:login.jsp";
+        }
+
+        int bookId = Integer.parseInt(request.getParameter("bookId"));
+        int userId = loggedIn.getUserId();
+
+        LoanDao loanDao = new LoanDao("bookrentalsystem");
+        BookDao bookDao = new BookDao("bookrentalsystem");
+
+            //Returning book to bookDao
+            bookDao.returnBook(bookId);
+            //Returning book to loanDao
+            loanDao.returnBook(userId, loanId);
+
+            //Removing loanId from the session
+            session.removeAttribute("loanId");
+
+            //
+            return "redirect:successfulReturn.jsp";
+
     }
 }
---
+++ src/main/webapp/successfulReturn.jsp
@@ -0,0 +1,34 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Evan Mc Donnell
+  Date: 07/01/2024
+  Time: 23:01
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <link rel="stylesheet" href="css/styles.css">
+    <title>Successful Return</title>
+</head>
+<body>
+<nav>
+    <a href="index.jsp">Home</a>
+    <a href="login.jsp">Login</a>
+    <a href="register.jsp">Register</a>
+</nav>
+
+<main>
+    <header>
+        <h1>
+            <%= "Book Rental System" %>
+        </h1>
+    </header>
+    <p>You have successfully returned your book, thank you!</p>
+</main>
+
+<footer>
+    &copy; 2024 Book Rental. All rights reserved.
+</footer>
+</body>
+</html>
