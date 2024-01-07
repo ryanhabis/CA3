6beaf4447045d54d465cc4d8ed1dd9c4c5d8e485
Heidi <heidimccrudden@gmail.com>
2024-01-07 17:31:33 +0000

    added many method headers to LoanDaoAdminInterface and LoanDaoInterface

--- src/main/java/CA3/bookRentalSystem/repositories/LoanDaoAdminInterface.java
+++ src/main/java/CA3/bookRentalSystem/repositories/LoanDaoAdminInterface.java
@@ -2,8 +2,18 @@ package CA3.bookRentalSystem.repositories;
 
 import CA3.bookRentalSystem.rental.Loan;
 
+import java.util.ArrayList;
+
 public interface LoanDaoAdminInterface extends LoanDaoInterface {
 
     public Loan getActiveLoanByBookIdAndUserId (int userId, int bookId);
+
+    public ArrayList<Loan> getAllLoans();
+    public ArrayList <Loan> getAllActiveLoans();
+    public ArrayList <Loan> getAllTerminatedLoans(); //optional, not needed
+
+    public ArrayList<Loan> getLoansByUser(int userId);
+    public ArrayList<Loan> getActiveLoansByUser(int userId); // and returnDate == null
+
 
 }
--- src/main/java/CA3/bookRentalSystem/repositories/LoanDaoInterface.java
+++ src/main/java/CA3/bookRentalSystem/repositories/LoanDaoInterface.java
@@ -1,5 +1,17 @@
 package CA3.bookRentalSystem.repositories;
 
+import CA3.bookRentalSystem.rental.Loan;
+
+import java.util.ArrayList;
+
 public interface LoanDaoInterface {
     public int createLoan (int userId, int bookId);
+    public ArrayList<Loan> getMyLoans(int userId);
+
+    public Loan getActiveLoanByBookId (int bookId, int userId); //find existing loans for the book in question
+
+    public Boolean checkOverdueFeePaid (int loanId, int userId); // get overdue loan by ID ->if returns not null, check if fee == 0?
+
+    public int returnBook (int loanId, int userId); //get loan by ID then set returnDate to the current date -> Loan Terminated
+    //user must use getAllActiveLoansByUser method to get the loan ID first
 }
