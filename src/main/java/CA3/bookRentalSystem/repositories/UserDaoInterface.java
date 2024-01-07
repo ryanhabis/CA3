d1f3b1e408645e6bcb5899f9a15cb5262757a10f
ryanhabis <ryan.habis@gmail.com>
2024-01-07 01:01:01 +0000

    Added javadocs and edited the read me file

--- src/main/java/CA3/bookRentalSystem/repositories/UserDaoInterface.java
+++ src/main/java/CA3/bookRentalSystem/repositories/UserDaoInterface.java
@@ -3,16 +3,51 @@
  */
 package CA3.bookRentalSystem.repositories;
 
+import CA3.bookRentalSystem.exceptions.DaoException;
 import CA3.bookRentalSystem.rental.User;
 
+import java.sql.SQLException;
 import java.time.LocalDate;
 
 public interface UserDaoInterface
 {
+    /**
+     * This method logs the user into his account if it's on the database.
+     * @param username checks if the username is in the database.
+     * @param password checks if the password is in the database.
+     * @return a boolean that checks if the user is registered or not.
+     *
+     * I used this code for reference.
+     * @referance https://stackoverflow.com/questions/35083680/check-if-record-exists-in-database-with-resultset
+     */
     public boolean login (String username, String password);
 
+    /**
+     * This method checks if the user is either an admin or a customer.
+     * @param username checks the username of the user
+     * @return the type of user the person is
+     * @throws DaoException
+     */
     public String getUserType(String username);
 //    public enum getUserType();
+
+    /**
+     * Retrieves a user from the database based on the provided username and password.
+     *
+     * @param username The username of the user to be retrieved.
+     * @param password The password associated with the specified username.
+     * @return A User object containing the user information if found, otherwise an empty User object.
+     * @throws RuntimeException If there is an issue closing the database resources.
+     * @throws DaoException If a SQL or DaoException occurs during the database operations.
+     */
     public User findUserByUsernameAndPassword(String username, String password);
+
+    /**
+     * Adds a new user to the database
+     *
+     * @return the new user if there account was made successfully or not
+     * @throws DaoException wil throw a run time exception
+     * @throws SQLException if the user was not added
+     */
     public int signup(String username, String password, String fName, String lName, LocalDate dob, String phoneNumber, String email, String addressLine1, String addressLine2, String city, String county, String eircode);
 }
--- src/main/java/CA3/bookRentalSystem/repositories/UserDaoInterfaceAdmin.java
+++ src/main/java/CA3/bookRentalSystem/repositories/UserDaoInterfaceAdmin.java
@@ -6,17 +6,37 @@ package CA3.bookRentalSystem.repositories;
 import CA3.bookRentalSystem.exceptions.DaoException;
 import CA3.bookRentalSystem.rental.User;
 
+import java.sql.SQLException;
 import java.time.LocalDate;
 import java.util.ArrayList;
 
 public interface UserDaoInterfaceAdmin extends UserDaoInterface
 {
     // Admin privileges
+
+    /**
+     * This method goes through all the users that are in the database and displays them
+     * @throws DaoException wil throw a run time exception
+     * @throws SQLException
+     * @return all the users on the database
+     */
     public ArrayList<User> getALLUsers();
 
+
+    /**
+     * This method checks if the user is either an admin or a customer.
+     * @param username checks the username of the user
+     * @return the type of user the person is
+     * @throws DaoException
+     */
     public String getUserType(String username);
 
     public boolean checkAccountEnabled(int userId);
+    /**
+     * This method removes a user from the databasse when the username is provided.
+     * @param username checks to see if the user is located on the database
+     * @return the user that was deleted or not
+     */
     public int removeUser(String username) throws DaoException;
 
 }
